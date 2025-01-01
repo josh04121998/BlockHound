5a5848816d11712228574272ec54b2aec8dddee1
Josh Handley <josh.handley@icloud.com>
2025-01-01 14:32:59 +0000

    new server and react

---
+++ server/server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3010;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
