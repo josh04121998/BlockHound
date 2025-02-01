be2a1c3dcd5baba8f37e8d8d25b4c4787b0deeff
Josh Handley <josh.handley@icloud.com>
2025-02-01 17:21:40 +0000

    NEXT_PUBLIC_SUPABASE_URL

--- api/webhook.js
+++ api/webhook.js
@@ -3,7 +3,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URLL,
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
